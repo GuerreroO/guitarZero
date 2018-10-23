847c7d49ecb88b747663c044d8ace733ebe49702
Student_09 <Student_09@ool-ad02067c.dyn.optonline.net>
2018-10-23 16:01:15 -0400

    added QWERTY controls. Added range conditional in which console.logs message what key is pressed with the range of div position

---
+++ main.js
@@ -0,0 +1,106 @@
+const board = document.querySelector('#board');
+const startBtn = document.querySelector('#start');
+const hitBox = document.querySelector('hitBox');
+let pos = 0;
+//Start game function
+function start(){
+  startBtn.addEventListener('click', renderGrid);
+}
+
+//Render Grid Function
+function renderGrid(){
+  for(let i = 0; i < 72; i+=1){
+    grid = document.createElement('div');
+    grid.className = 'grid';
+    // grid.dataset.index = i;
+    board.appendChild(grid);
+  }
+  for(let i = 0; i < 6; i += 1){
+    grid = document.createElement('div');
+    grid.className = 'hitBox';
+    board.appendChild(grid);
+  }
+  renderNote();
+}
+
+//Render Note function
+function renderNote(){
+  note = document.createElement('div');
+  note.className = "note";
+  board.appendChild(note);
+  let id = setInterval(notes, 100);
+  setInterval(notes, 100);
+  function notes(){
+    if(pos === 650){
+      clearInterval(id);
+    }
+    else{
+      //Game Controls
+      document.querySelector('body').addEventListener('keydown', (e) => {
+        const keyCode = e.keyCode;
+        switch(keyCode){
+          case 81: //Q
+            console.log('Q');
+            if(pos >= 400 && pos <= 500){
+              console.log('you did it!');
+            }
+            break;
+          case 87: //W
+            console.log('W');
+            break;
+          case 69: //E
+            console.log('E');
+            break;
+          case 82: //R
+            console.log('R');
+            break;
+          case 84: //T
+            console.log('T');
+            break;
+          case 89: //Y
+            console.log('Y');
+            break;
+        }
+      });
+      console.log(pos);
+      pos += 10;
+      note.style.top = pos + 'px';
+    }
+  }
+}
+
+
+
+// checkAccuracy{
+//   if()
+// }
+
+//Game Controls
+document.querySelector('body').addEventListener('keydown', (e) => {
+  const keyCode = e.keyCode;
+  switch(keyCode){
+    case 81: //Q
+      console.log('Q');
+      break;
+    case 87: //W
+      console.log('W');
+      break;
+    case 69: //E
+      console.log('E');
+      break;
+    case 82: //R
+      console.log('R');
+      break;
+    case 84: //T
+      console.log('T');
+      break;
+    case 89: //Y
+      console.log('Y');
+      break;
+  }
+});
+
+
+
+
+start();
