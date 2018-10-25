93b16d6b6f92aafa64f19990e5af29d72136d129
Student_09 <Student_09@ool-ad02067c.dyn.optonline.net>
2018-10-25 17:14:26 -0400

    release multtple sets of divs, can remova a set with q

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const board = document.querySelector('#board');
 const startBtn = document.querySelector('#start');
 const hitBox = document.querySelector('hitBox');
+let counter = 0;
+
 let pos = 0;
 let score = 0;
 let i = 0;
@@ -17,7 +19,7 @@ function renderGrid(){
     // grid.dataset.index = i;
     board.appendChild(grid);
   }
-  for(let i = 0; i < 6; i += 1){
+  for(let i = 0; i < 12; i += 1){
     grid = document.createElement('div');
     grid.className = 'Btn[i] hitBox';
     board.appendChild(grid);
@@ -27,66 +29,76 @@ function renderGrid(){
 
 //Render Note function
 function renderNote(){
-  let id = setInterval(notes, 100);
+  let id = setInterval(notes, 200);
+  function notes(){
+    let noteClass = document.querySelectorAll('.note');
+    let note0 = document.querySelector('#note0') // noteClass= divs with class of .note
+    console.log(counter, 'this is');
+    if(counter === 100){
+      clearInterval(id);
+    }
+    else{
+      //Game Controls
+      document.querySelector('body').addEventListener('keydown', (e) => {
+        const keyCode = e.keyCode;
+        switch(keyCode){
+          case 81: //Q
+            console.log('Q');
+            if(pos >= 400 && pos <= 500){
+              pos = 0;
+              score += 100;
+              console.log(note0, "whattup")
+              noteClass.remove(note0); //need to give individual Id's to all divs
+              clearInterval(id);
+              console.log('you did it!');
+            }
+            else{
+
+            }
+            break;
+          case 87: //W
+            console.log('W');
+            break;
+          case 69: //E
+            console.log('E');
+            break;
+          case 82: //R
+            console.log('R');
+            break;
+          case 84: //T
+            console.log('T');
+            break;
+          case 89: //Y
+            console.log('Y');
+            break;
+        }
+      });
+
+      //Note production
+      // console.log(pos);
+      setTimeout(randomNote(), 100);
+
+      let noteId = document.querySelector('#note0'); //noteId = first note0
+      if(counter === 0){randomNote();}
+      if(counter === 5){randomNote();} // runs function random note
+      pos += 10;
+      noteClass.forEach(e => { e.style.top = pos + 'px'; });
+      counter++;
+    }
+
+  }
   // return score;
 }
 
-function notes(){
-  if(pos === 650){
-    clearInterval(id);
-  }
-  else{
-    //Game Controls
-    document.querySelector('body').addEventListener('keydown', (e) => {
-      const keyCode = e.keyCode;
-      switch(keyCode){
-        case 81: //Q
-          console.log('Q');
-          if(pos >= 400 && pos <= 500){
-            currentPos = pos;
-            score += 100;
-            noteId.remove();
-            console.log('you did it!');
-          }
-          else{
 
-          }
-          break;
-        case 87: //W
-          console.log('W');
-          break;
-        case 69: //E
-          console.log('E');
-          break;
-        case 82: //R
-          console.log('R');
-          break;
-        case 84: //T
-          console.log('T');
-          break;
-        case 89: //Y
-          console.log('Y');
-          break;
-      }
-    });
 
-    //Note production
-    console.log(pos);
-    setTimeout(randomNote(), 1000);
-    let noteClass = document.querySelectorAll('.note'); // noteClass= divs with class of .note
-    let noteId = document.querySelector('#note0'); //noteId = first note0
-    randomNote(); // runs function random note
-    pos += 10;
-    noteClass.forEach(note => note.style.top = pos + 'px');
-  }
-}
 
 function randomNote(){ //NoteProduction
   for(let i = 0; i < 6; i +=1){
-    note = document.createElement('div'); //note div is created
+    let note = document.createElement('div'); //note div is created
+    note.id = `note${i}`;
     whichColumn(note); // run whichColumn function
-    note.dataset.delay = Math.floor(Math.random()*6) * 1000; // adds data-delay attribute with a value between 1000-10000
-    console.log('this is note.dataset.delay', note.dataset.delay)
+    // console.log('this is note.dataset.delay', note.dataset.delay)
     board.appendChild(note); // the note is appended to the board
     // setTimeout((function goes here), note.dataset.delay)
   }
@@ -117,6 +129,9 @@ function whichColumn(note){
   }
 }
 
-// function dropNote
+function dropNote(){
+  currentPos = pos
+
+}
 
 start();
