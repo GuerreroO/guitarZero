e5c3d21dcb01f304b722dea4986252dbcb41d8ef
Student_09 <Student_09@ool-ad02067c.dyn.optonline.net>
2018-10-26 10:07:12 -0400

    moved things around, now single blocks fall, but then more begin to appear

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const board = document.querySelector('#board');
 const startBtn = document.querySelector('#start');
 const hitBox = document.querySelector('hitBox');
-let counter = 0;
 
 let pos = 0;
 let score = 0;
 let i = 0;
+
 //Start game function
 function start(){
   startBtn.addEventListener('click', renderGrid);
@@ -29,12 +29,18 @@ function renderGrid(){
 
 //Render Note function
 function renderNote(){
-  let id = setInterval(notes, 200);
+  let counter = 0;
+  let id = setInterval(notes, 20);
   function notes(){
     let noteClass = document.querySelectorAll('.note');
     let note0 = document.querySelector('#note0') // noteClass= divs with class of .note
     console.log(counter, 'this is');
-    if(counter === 100){
+    if(counter === 0){randomNote()}
+    let noteId = document.querySelector('#note0'); //noteId = first note
+    pos += 10;
+    noteClass.forEach(e => { e.style.top = pos + 'px'; });
+    counter++;
+    if(counter === 1000){
       clearInterval(id);
     }
     else{
@@ -44,11 +50,12 @@ function renderNote(){
         switch(keyCode){
           case 81: //Q
             console.log('Q');
-            if(pos >= 400 && pos <= 500){
+            if(pos >= 600 && pos <= 650){
               pos = 0;
               score += 100;
-              console.log(note0, "whattup")
-              noteClass.remove(note0); //need to give individual Id's to all divs
+              randomNote();
+              console.log(noteClass, "whattup")
+              noteClassremove(); //need to give individual Id's to all divs
               clearInterval(id);
               console.log('you did it!');
             }
@@ -58,53 +65,82 @@ function renderNote(){
             break;
           case 87: //W
             console.log('W');
+            if(pos >= 600 && pos <= 650){
+              pos = 0;
+              score += 100;
+              randomNote();
+              console.log(noteClass, "whattup")
+              noteClass.remove(); //need to give individual Id's to all divs
+              // clearInterval(id);
+              console.log('you did it!');
+              }
             break;
           case 69: //E
             console.log('E');
+            if(pos >= 600 && pos <= 650){
+              pos = 0;
+              score += 100;
+              randomNote();
+              console.log(noteClass, "whattup")
+              noteClass.remove(); //need to give individual Id's to all divs
+              // clearInterval(id);
+              console.log('you did it!');
+            }
             break;
           case 82: //R
             console.log('R');
+            if(pos >= 600 && pos <= 650){
+              pos = 0;
+              score += 100;
+              randomNote();
+              console.log(noteClass, "whattup")
+              noteClass.remove(); //need to give individual Id's to all divs
+              // clearInterval(id);
+              console.log('you did it!');
+            }
             break;
           case 84: //T
             console.log('T');
+            if(pos >= 600 && pos <= 650){
+              pos = 0;
+              score += 100;
+              randomNote();
+              console.log(noteClass, "whattup")
+              noteClass.remove(); //need to give individual Id's to all divs
+              // clearInterval(id);
+              console.log('you did it!');
+            }
             break;
           case 89: //Y
             console.log('Y');
+            if(pos >= 600 && pos <= 650 && note.className === 'orangeNote'){
+              pos = 0;
+              score += 100;
+              randomNote();
+              console.log(noteClass, "whattup")
+              noteClass.remove(); //need to give individual Id's to all divs
+              // clearInterval(id);
+              console.log('you did it!');
+            }
             break;
         }
       });
-
-      //Note production
-      // console.log(pos);
-      setTimeout(randomNote(), 100);
-
-      let noteId = document.querySelector('#note0'); //noteId = first note0
-      if(counter === 0){randomNote();}
-      if(counter === 5){randomNote();} // runs function random note
-      pos += 10;
-      noteClass.forEach(e => { e.style.top = pos + 'px'; });
-      counter++;
     }
-
   }
   // return score;
 }
 
-
-
-
 function randomNote(){ //NoteProduction
-  for(let i = 0; i < 6; i +=1){
+  // for(let i = 0; i < 6; i +=1){
     let note = document.createElement('div'); //note div is created
-    note.id = `note${i}`;
+    note.id = `note${i}`; // note is given an id of "note[i]"
     whichColumn(note); // run whichColumn function
     // console.log('this is note.dataset.delay', note.dataset.delay)
     board.appendChild(note); // the note is appended to the board
     // setTimeout((function goes here), note.dataset.delay)
-  }
+  // }
 }
 
-//I need a function that will send random divs out at random moments
 function whichColumn(note){
   note.style.left = (Math.floor(Math.random()*6)) * 60 + 'px'; //note[i] is given a style of left with it's value randomy generated
   switch(note.style.left){
